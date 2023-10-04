166b082febda0c1b1a23dd6d4d0241e2e3ca98a4
Ramiro Bahena <ramiro.bahena@ceneval.edu.mx>
2023-10-04 17:13:16 -0600

    Agregar ruts hija para dashbiard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AuthModule } from './modules/auth/auth.module';
 import { HomeModule } from './modules/home/home.module';
 import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './modules/home/pages/home/home.component';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 
 const routes: Routes = [
   {
@@ -11,6 +12,12 @@ const routes: Routes = [
     loadChildren: () =>
       import('./modules/auth/auth.module').then((module) => AuthModule),
   },
+  {
+    path: 'panel-trabajo',
+    loadChildren: () =>
+      import('./modules/dashboard/dashboard.module').then((module) => DashboardModule),
+  },
+
   { path: '**', component: HomeComponent },
 ];
 
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,17 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { InventaryComponent } from './pages/inventary/inventary.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-
-
+import { EntradasComponent } from './components/entradas/entradas.component';
+import { SalidasComponent } from './components/salidas/salidas.component';
+import { AlmacenComponent } from './components/almacen/almacen.component';
+import { DashboardRoutingModule } from './dashboard-routing.module';
 
 @NgModule({
-  declarations: [
-    SidebarComponent,
-    InventaryComponent
-  ],
-  imports: [
-    CommonModule,
-    SharedModule
-  ]
+  declarations: [SidebarComponent, InventaryComponent, EntradasComponent, SalidasComponent, AlmacenComponent],
+  imports: [CommonModule, SharedModule, DashboardRoutingModule],
+  exports:[EntradasComponent, SalidasComponent, AlmacenComponent]
+
 })
-export class DashboardModule { }
+export class DashboardModule {}
