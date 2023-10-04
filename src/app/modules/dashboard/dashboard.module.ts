9ce849a8a642e12e4931526e24abe6f18641f046
Ramiro Bahena <ramiro.bahena@ceneval.edu.mx>
2023-10-04 16:45:47 -0600

    actualizar sidebar component

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarComponent } from './pages/sidebar/sidebar.component';
 import { InventaryComponent } from './pages/inventary/inventary.component';
 
 
