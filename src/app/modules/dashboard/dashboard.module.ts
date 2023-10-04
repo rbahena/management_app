8826eaf2fe6115e43b151c86d91e99bd23d5b16c
Ramiro Bahena <ramiro.bahena@ceneval.edu.mx>
2023-10-04 17:00:34 -0600

    Agregar imports

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,27 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from './pages/sidebar/sidebar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { InventaryComponent } from './pages/inventary/inventary.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { EntradasComponent } from './components/entradas/entradas.component';
-import { SalidasComponent } from './components/salidas/salidas.component';
-import { AlmacenComponent } from './components/almacen/almacen.component';
-import { ProveedoresComponent } from './pages/proveedores/proveedores.component';
-import { ProductosComponent } from './pages/productos/productos.component';
-import { CategoriasComponent } from './pages/categorias/categorias.component';
 
 
 
 @NgModule({
   declarations: [
     SidebarComponent,
-    InventaryComponent,
-    EntradasComponent,
-    SalidasComponent,
-    AlmacenComponent,
-    ProveedoresComponent,
-    ProductosComponent,
-    CategoriasComponent
+    InventaryComponent
   ],
   imports: [
     CommonModule,
