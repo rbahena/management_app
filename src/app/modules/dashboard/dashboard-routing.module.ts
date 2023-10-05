163dc6da89e5c1303e84903d8daeaedc2ce95f82
Ramiro Bahena <ramiro.bahena@ceneval.edu.mx>
2023-10-04 18:24:12 -0600

    Agregar side bar

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -4,6 +4,7 @@ import { EntradasComponent } from './components/entradas/entradas.component';
 import { SalidasComponent } from './components/salidas/salidas.component';
 import { AlmacenComponent } from './components/almacen/almacen.component';
 import { RouterModule, Routes } from '@angular/router';
+import { InventaryComponent } from './pages/inventary/inventary.component';
 
 const routes: Routes = [
   {
@@ -12,7 +13,7 @@ const routes: Routes = [
       { path: 'entradas', component: EntradasComponent },
       { path: 'salidas', component: SalidasComponent },
       { path: 'almacen', component: AlmacenComponent },
-      { path: '**', component: AlmacenComponent },
+      { path: '**', component: InventaryComponent },
     ],
   },
 ];
